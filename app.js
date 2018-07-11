fbd0cf1a6f81a33fb0e7e6d76a7037ef6559a9d1
Daniel González <daniel.gv.1993@gmail.com>
2018-07-11 12:19:50 -0500

    Test #2 Fixed tokens

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const BootBot = require('bootbot');
 
 const bot = new BootBot({
-  accessToken: 'FB_ACCESS_TOKEN',
-  verifyToken: 'FB_VERIFY_TOKEN',
-  appSecret: 'FB_APP_SECRET'
+  accessToken: process.env.PAGE_ACCESS_TOKEN,
+  verifyToken: process.env.VERIFICATION_TOKEN,
+  appSecret: '8af20da7078a094599b9c203bfd6d6db'
 });
 
 bot.on('message', (payload, chat) => {
