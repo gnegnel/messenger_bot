a4cfec36312afd346ebede2bb628142eecbdba21
Daniel González <daniel.gv.1993@gmail.com>
2018-07-12 17:35:15 -0500

    Test #11 bot attachment

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ const bot = new BootBot({
 bot.on('message', (payload, chat) => {
   const text = payload.message.text;
   console.log(payload);
-  chat.say(`${text}`);
+  chat.say('https://scontent.fntr3-1.fna.fbcdn.net/v/t1.15752-9/36551449_888024924703554_4952352232956755968_n.jpg?_nc_cat=0&oh=f636f46dd921b7d5ce1764ac57fdfe8a&oe=5BA705E7
+https://scontent.fntr3-1.fna.fbcdn.net/v/t1.15752-9/36551449_888024924703554_4952352232956755968_n.jpg?_nc_cat=0&oh=f636f46dd921b7d5ce1764ac57fdfe8a&oe=5BA705E7');
 });
 
 bot.on('attachment',(payload,chat)=>{
-  console.log("a", payload.message.attachments[0]);
-  console.log("b", payload.message.attachments[1]);
-  console.log("c", payload.message.attachments);
+  console.log("a", payload.message.attachments[0].payload.url);
   chat.say('Me mandaste un attachment');
 });
 
